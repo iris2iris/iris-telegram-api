1a1cc7e08685b3bee4538511aa04727afec4b4c9
iris2iris <33526313+iris2iris@users.noreply.github.com>
2022-02-21 11:44:48 +0400

    Major change in multithread read-write operating.

--- src/iris/tg/TgReadWriteBufferDefault.kt
+++ src/iris/tg/TgReadWriteBufferDefault.kt
@@ -8,16 +8,20 @@ import kotlin.collections.ArrayList
  * @created 02.11.2020
  * @author [Ivan Ivanov](https://t.me/irisism)
  */
-open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private val maxTimeToWrite: Long = 10L) : TgReadWriteUpdateBuffer<T> {
+@Suppress("MemberVisibilityCanBePrivate")
+open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private val maxTimeToWrite: Long = WAIT_TIME_DONT_WAIT) : TgReadWriteUpdateBuffer<T> {
 
 	protected var queue: ArrayDeque<T> = ArrayDeque(maxQueueSize)
 
 	private val queueModifyLock = Object()
 	private val notEmpty = Object()
 	private val notFull = Object()
-	private val noSuccessLock = Object()
 
 	companion object {
+
+		const val WAIT_TIME_FOREVER = 0L
+		const val WAIT_TIME_DONT_WAIT = -1L
+
 		private val logger = Logger.getLogger("iris.tg")
 	}
 
@@ -32,34 +36,55 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 		}
 
 		if (!success) {
-			synchronized(noSuccessLock) {
-				val startTime = System.currentTimeMillis()
-				var timeToWait = maxTimeToWrite
-				do {
-					synchronized(notFull) { notFull.wait(timeToWait) }
-					synchronized(queueModifyLock) {
-						if (queue.size < maxQueueSize) {
-							queue += event
-							synchronized(notEmpty) { notEmpty.notify() }
-							return
+			//synchronized(noSuccessLock) {
+				if (maxTimeToWrite > 0) { // процесс ожидания
+					val startTime = System.currentTimeMillis()
+					var timeToWait = maxTimeToWrite
+					while (timeToWait > 0L) {
+						synchronized(notFull) { notFull.wait(timeToWait) }
+						synchronized(queueModifyLock) {
+							if (queue.size < maxQueueSize) {
+								queue += event
+								synchronized(notEmpty) { notEmpty.notify() }
+								return
+							}
+						}
+						timeToWait = maxTimeToWrite + startTime - System.currentTimeMillis()
+					}
+				} else if (maxTimeToWrite == 0L) {
+					// TODO: наверно, здесь лучше с isInterrupted?
+					while (true) {
+						synchronized(notFull) { notFull.wait(0L) }
+						synchronized(queueModifyLock) {
+							if (queue.size < maxQueueSize) {
+								queue += event
+								synchronized(notEmpty) { notEmpty.notify() }
+								return
+							}
 						}
 					}
-					timeToWait = maxTimeToWrite + startTime - System.currentTimeMillis()
-				} while (timeToWait > 0L)
+				}
 
 				synchronized(queueModifyLock) {
 					if (queue.size >= maxQueueSize) {
 						queueIsFull()
-						queue += event
-						synchronized(notEmpty) { notEmpty.notify() }
-						synchronized(notFull) { notFull.notify() }
+						if (queue.size < maxQueueSize) {
+							queue += event
+							synchronized(notEmpty) { notEmpty.notify() }
+							if (queue.size < maxQueueSize)
+								synchronized(notFull) { notFull.notify() }
+						}
 					}
 				}
-			}
+			//}
 		}
 	}
 
 	override fun write(events: List<T>) {
+		write(events, 0)
+	}
+
+	private fun write(events: List<T>, depth: Int) {
 		val eventsSize = events.size
 		val wroteSize = synchronized(queueModifyLock) {
 			addQueue(events)
@@ -67,33 +92,67 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 
 		if (wroteSize < eventsSize) {
 			if (wroteSize == 0) {
-				val subWroteSize = synchronized(noSuccessLock)t@ {
-					val maxWaitTime = 10L
-					val startTime = System.currentTimeMillis()
-					var timeToWait = maxWaitTime
-					do {
-						synchronized(notFull) { notFull.wait(timeToWait) }
-						synchronized(queueModifyLock) {
-							if (queue.size < maxQueueSize) {
-								return@t addQueue(events)
+				val subWroteSize = run t@ {
+					if (maxTimeToWrite > 0) {
+						val startTime = System.currentTimeMillis()
+						var timeToWait = maxTimeToWrite
+						while (timeToWait > 0L) {
+							synchronized(notFull) { notFull.wait(timeToWait) }
+							synchronized(queueModifyLock) {
+								val res = addQueue(events)
+								if (res != 0)
+									return@t res
+							}
+							timeToWait = maxTimeToWrite + startTime - System.currentTimeMillis()
+						}
+					} else if (maxTimeToWrite == 0L) {
+						// TODO: наверно, здесь лучше с isInterrupted?
+						while (true) {
+							synchronized(notFull) { notFull.wait(0L) }
+							synchronized(queueModifyLock) {
+								val res = addQueue(events)
+								if (res != 0)
+									return@t res
 							}
 						}
-						timeToWait = maxWaitTime + startTime - System.currentTimeMillis()
-					} while (timeToWait > 0L)
+					}
 
 					synchronized(queueModifyLock) {
 						if (queue.size >= maxQueueSize) {
 							queueIsFull()
-							synchronized(notFull) { notFull.notify() }
+							// перепроверяем размер очереди, потому что queueIsFull может быть переопределена и не сбрасывать очередь
+							if (queue.size < maxQueueSize) {
+								val res = addQueue(events)
+								if (res != 0) {
+									if (queue.size < maxQueueSize)
+										synchronized(notFull) { notFull.notify() }
+									return@t res
+								}
+								synchronized(notFull) { notFull.notify() }
+							}
 						}
 					}
 					0
 				}
-				if (subWroteSize < eventsSize)
-					write(if (subWroteSize == 0) events else events.subList(subWroteSize, eventsSize))
-			} else
-				write(events.subList(wroteSize, eventsSize))
+				if (subWroteSize < eventsSize) {
+					val eventsLeft = if (subWroteSize == 0) events else events.subList(subWroteSize, eventsSize)
+					if (depth < 10)
+						write(eventsLeft, depth + 1)
+					else
+						eventsRejected(eventsLeft)
+				}
+			} else {
+				val eventsLeft = events.subList(wroteSize, eventsSize)
+				if (depth < 10)
+					write(eventsLeft, depth + 1)
+				else
+					eventsRejected(eventsLeft)
+			}
 		}
+	}
+
+	protected open fun eventsRejected(events: List<T>) {
+		logger.warning { "Events rejected: ${events.size}" }
 	}
 
 	private fun addQueue(events: List<T>): Int {
@@ -108,9 +167,9 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 			}
 
 			else -> { // когда частично влазит, частично переполнено
-				val wroteSize = maxQueueSize - size
-				queue += events.subList(0, wroteSize)
-				wroteSize
+				val writeSize = maxQueueSize - size
+				queue += events.subList(0, writeSize)
+				writeSize
 			}
 		}
 		synchronized(notEmpty) { notEmpty.notify() }
@@ -122,7 +181,7 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 	}
 
 	open fun queueIsFull() {
-		logger.warning { "Callback API queue is full (${queue.size} elements). Clearing..." }
+		logger.warning { "Queue is full (${queue.size} elements). Clearing..." }
 		queue.clear()
 	}
 
@@ -133,7 +192,7 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 	override fun read(limit: Int, wait: Boolean): List<T> {
 
 		do {
-			synchronized(queueModifyLock) {
+			val res = synchronized(queueModifyLock) {
 				val queue = this.queue
 				if (queue.size != 0) {
 					val res = if (limit == 0 || queue.size <= limit) {
@@ -145,9 +204,14 @@ open class TgReadWriteBufferDefault<T>(private val maxQueueSize: Int, private va
 							a += queue.removeFirst()
 						a
 					}
-					synchronized(notFull) { notFull.notify() }
-					return res
-				}
+
+					res
+				} else
+					null
+			}
+			if (res != null) {
+				synchronized(notFull) { notFull.notify() }
+				return res
 			}
 
 			if (!wait)
